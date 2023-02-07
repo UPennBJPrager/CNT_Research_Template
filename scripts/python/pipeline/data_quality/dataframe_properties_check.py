45927b54c8250bd6fd4d3bad3e76a55092eeaf3d
Brian P <bjprager@seas.upenn.edu>
2023-02-07 10:05:36 -0500

    Added data quality checks to pipeline. Building out example user script to connect components.

--- scripts/python/pipeline/data_quality/dataframe_properties_check.py
+++ scripts/python/pipeline/data_quality/dataframe_properties_check.py
@@ -1,1 +1,44 @@
-# Standard library importimport osimport sys# Add submodule repository tools to the current opathtestpath = '../../../../unit_tests'if testpath not in sys.path:    sys.path.append(testpath)# User library importimport array_unit_tests as AUT def main(DF,N,M=-1,dtype='float64',verbose=True):    """    Ensure dataframe properties are correct. Main function runs all checks.    Parameters    ----------    DF : Pandas dataframe        Dataframe contain ieeg data. SamplesxChannel format.    N : Integer        Number of channels expected.    M : Integer, optional        Number of expected samples. Set to -1 to skip this check. The default is -1.    dtype : String, optional        Expected datatype for the dataframe entries.    verbose : TYPE, optional        Print test results to stdout. The default is True.    Returns    -------    Boolean flag.    """        # Run through all tests    CLS = AUT.TestArrayProperties(DF,verbose)    CLS.test_dim(M,N)    CLS.test_type(dtype)    CLS.test_inf()    CLS.test_nan()if __name__ == '__main__':    main()
+# Standard library import
+import os
+import sys
+
+# Add submodule repository tools to the current opath
+testpath = '../../../../unit_tests'
+if testpath not in sys.path:
+    sys.path.append(testpath)
+
+# User library import
+import array_unit_tests as AUT 
+
+def main(DF,N,M=-1,dtype='float64',verbose=True):
+    """
+    Ensure dataframe properties are correct. Main function runs all checks.
+
+    Parameters
+    ----------
+    DF : Pandas dataframe
+        Dataframe contain ieeg data. SamplesxChannel format.
+    N : Integer
+        Number of channels expected.
+    M : Integer, optional
+        Number of expected samples. Set to -1 to skip this check. The default is -1.
+    dtype : String, optional
+        Expected datatype for the dataframe entries.
+    verbose : TYPE, optional
+        Print test results to stdout. The default is True.
+
+    Returns
+    -------
+    Boolean flag.
+
+    """
+    
+    # Run through all tests
+    CLS = AUT.TestArrayProperties(DF,verbose)
+    CLS.test_dim(M,N)
+    CLS.test_type(dtype)
+    CLS.test_inf()
+    CLS.test_nan()
+
+if __name__ == '__main__':
+    main()
--- scripts/python/users/bjprager/sample_ieeg_workflow.py
+++ scripts/python/users/bjprager/sample_ieeg_workflow.py
@@ -1,1 +1,43 @@
-import sysimport globimport argparsefor dirs in glob.glob("../../pipeline/**"):    if dirs not in sys.path:        sys.path.append(dirs)        # User library importimport pipeline_datapull_ieeg as PDIimport dataframe_properties_check as DPCdef main():    """    Calls series of commands for a simple ieeg pipeline.    Returns    -------    None.    """        # Command line options needed to obtain data.    parser = argparse.ArgumentParser()    parser.add_argument('-u', '--user', required=True, help='username')    parser.add_argument('-p', '--password', help='password (will be prompted if omitted)')    parser.add_argument('--dataset', help='dataset name')    parser.add_argument('--start', type=int, help='start offset in usec')    parser.add_argument('--duration', type=int, help='number of usec to request')    parser.add_argument('--local_path', default=None, type=str, help='Path to local data to ingest manually. Default=None.')    parser.add_argument('--silent', dest='verbose', default=True, action='store_false', help='Silent Verbose Output. Default=False.')    args = parser.parse_args()        # Data ingestion    DF,fs = PDI.main(args)        # Data quality check    quality_flag = DPC.main(DF,16,verbose=args.verbose)        return DF,fsif __name__ == '__main__':    DF,fs = main()
+import sys
+import glob
+import argparse
+
+for dirs in glob.glob("../../pipeline/**"):
+    if dirs not in sys.path:
+        sys.path.append(dirs)
+        
+# User library import
+import pipeline_datapull_ieeg as PDI
+import dataframe_properties_check as DPC
+
+def main():
+    """
+    Calls series of commands for a simple ieeg pipeline.
+
+    Returns
+    -------
+    None.
+
+    """
+    
+    # Command line options needed to obtain data.
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-u', '--user', required=True, help='username')
+    parser.add_argument('-p', '--password', help='password (will be prompted if omitted)')
+    parser.add_argument('--dataset', help='dataset name')
+    parser.add_argument('--start', type=int, help='start offset in usec')
+    parser.add_argument('--duration', type=int, help='number of usec to request')
+    parser.add_argument('--local_path', default=None, type=str, help='Path to local data to ingest manually. Default=None.')
+    parser.add_argument('--silent', dest='verbose', default=True, action='store_false', help='Silent Verbose Output. Default=False.')
+    args = parser.parse_args()
+    
+    # Data ingestion
+    DF,fs = PDI.main(args)
+    
+    # Data quality check
+    quality_flag = DPC.main(DF,16,verbose=args.verbose)
+    
+    return DF,fs
+
+if __name__ == '__main__':
+    DF,fs = main()
